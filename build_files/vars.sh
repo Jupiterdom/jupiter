e2d402a5cc71b2a782624fb777e6a468aeb0a20d
rvsmooth <riveeks.smooth@gmail.com>
2025-12-10 07:53:37 +0530

    initial jupiter build system

---
+++ build_files/vars.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -ouex pipefail
+
+TIMESTAMP="$(date +"%m%d%Y%H%M%S")"
+FEDORA_MAJOR_VERSION="43"
+
+vars=(
+  BASE_IMAGE_NAME="kinoite"
+  IMAGE_NAME="Jupiter-kinoite"
+  IMAGE_PRETTY_NAME="Jupiter"
+  CODE_NAME="Jupiter-terraformed"
+  IMAGE_LIKE="fedora"
+  HOME_URL="https://github.com/Jupiterdom/jupiter"
+  DOCUMENTATION_URL="https://github.com/Jupiterdom/jupiter"
+  SUPPORT_URL="https://github.com/Jupiterdom/Jupiter/issues/"
+  BUG_SUPPORT_URL="https://github.com/Jupiterdom/Jupiter/issues/"
+  VERSION="${FEDORA_MAJOR_VERSION}-${TIMESTAMP}"
+  DEFAULT_TAG="stable"
+)
+
+for var in ${vars[@]}; do
+  set +u
+  echo "$var" >>"$GITHUB_ENV" || true
+  set -u
+  export "$var"
+done
